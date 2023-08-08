2b69d70116681fb4bef52913b85d115262ec95d7
partibrejkers <brejkersi@gmail.com>
2023-08-08 12:28:10 +0200

    C++ sa komentarima

---
+++ 01uvod/p2v2.cpp
@@ -0,0 +1,7 @@
+#include<iostream> // UI biblioteka
+
+// moj prvi C++ program sa komentarima
+
+int main() { // Glavna funkcija
+	std::cout << "Hello, world!++" << std::endl; // ispis
+	return 0; } // OK
