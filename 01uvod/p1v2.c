6ba580e6d514ce7b7b2db05d964e278c892a1c9f
partibrejkers <brejkersi@gmail.com>
2023-08-08 12:19:05 +0200

    u prvi program dodati komentari...

---
+++ 01uvod/p1v2.c
@@ -0,0 +1,7 @@
+/* prvi program, druga verzija... */
+#include<stdio.h> /* biblioteka za Input i Output, IO, stdIO */
+/* ovo je moj prvi C program, sa komentarima */
+
+int main(){
+	printf("Hello, world!\n");  /* prelazak u novi red */
+    return 0; } /* javlja OSu da je sve u redu */
