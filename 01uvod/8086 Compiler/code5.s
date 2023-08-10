19fdafc9f4c110cce741a005796970e99fd9ce76
partibrejkers <brejkersi@gmail.com>
2023-08-10 17:03:26 +0200

    Zastavice CF i ZF

--- 01uvod/8086 Compiler/code5.s
+++ 01uvod/8086 Compiler/code5.s
@@ -1,5 +1,7 @@
 ; CF, ZF
-
+; 128 i 128 je 256, kapacitet bajta je samo 255
+; tako da se dize zastavica za prekoracenje CF
+; zadnjih 8 bitova u resenju je 00000000, dize se i zastavica za nulu
 
 a: DB 128 ; a = 128(10) = 1000.0000(2)
 b: DB 128 ; b = 128(10) = 1000.0000(2)
