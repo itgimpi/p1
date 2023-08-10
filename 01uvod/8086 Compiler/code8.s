a2de178435c96672681d3459d69989c73dde28cc
partibrejkers <brejkersi@gmail.com>
2023-08-10 17:15:29 +0200

    opet negativni :(

--- 01uvod/8086 Compiler/code8.s
+++ 01uvod/8086 Compiler/code8.s
@@ -1,11 +1,18 @@
 ; 5 + -5, ZF = 1
+; kako se stvarno predstavljaju negativni brojevi
 
-
-a: DB 0x5 ;        a = 5 = 0000.0101
-;b: DB 0xfb ; b = -5, 5NK = 1111.1010, 5PK = 1111.1011 = fb
-;b: DB 0b11111011 ; b = -5, 5NK = 1111.1010, 5PK = 1111.1011 = fb 
-b: DB 251 ; b = -5, 5NK = 1111.1010, 5PK = 1111.1011 = fb 
+a:  DB 0x5 ;        a = 5 = 0000.0101
+;b: DB 0xfb ; b = -5, 5NK = 1111.1010, 5PK = 1111.1011 = fb         <- heksadekadno
+;b: DB 0b11111011 ; b = -5, 5NK = 1111.1010, 5PK = 1111.1011 = fb   <- binarno
+b: DB 251 ; b = -5, 5NK = 1111.1010, 5PK = 1111.1011 = fb           <- dekadno
+; 5 je 0000.0101
+; njegov nepotpuni komplement je 1111.1010
+; njegov potpuni komplement je 1111.1010 + 1 = 1111.1011
+; ako je broj negativan, to je -5
+; ako je pozitivan, to je 251
+; zanimljivo je da se poz. 251 i neg. -5 u racunaru predstavljaju potpuno isto!
 c: DB 0 ; c = 0, Write into RAM, on Address &c
+; 0000.0101 + 1111.1011 = CF->1 .0000.0000<-ZF
 
 start:
 
