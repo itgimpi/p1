cdb1dc6d3574fff694fbf50adfa519442d7a8cd0
partibrejkers <brejkersi@gmail.com>
2023-08-10 15:27:13 +0200

    Zastavice CF i ZF

---
+++ 01uvod/8086 Compiler/code5.s
@@ -0,0 +1,19 @@
+; CF, ZF
+
+
+a: DB 128 ; a = 128(10) = 1000.0000(2)
+b: DB 128 ; b = 128(10) = 1000.0000(2)
+;                C ->   1.0000.0000 -> 00
+c: DB 0 ; c = 0, Write into RAM, on Address &c
+
+start:
+
+    mov AL, byte a ; Move from RAM into CPU, into Reg. A
+    mov BL, byte b ; Move from RAM into CPU, into Reg. B
+          
+    add AL, BL; saberi A + B, zbir upisi u 1 bajt A registra
+    mov byte c, AL ; Move from CPU, Reg. B, into RAM, address &c
+    ;zbir se upise u RAM, svi podaci su u RAMu
+
+    mov AH, 0x13 ; kao return 0 u C-u
+    int 0x10  ; BIOS interrupt
