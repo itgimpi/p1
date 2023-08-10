1475c5db9ae9e3f512f100920aa14901d6127da0
partibrejkers <brejkersi@gmail.com>
2023-08-10 17:19:08 +0200

    zastavica za znak -

--- 01uvod/8086 Compiler/code9.s
+++ 01uvod/8086 Compiler/code9.s
@@ -1,8 +1,10 @@
 ; SF = 1
-
+; Zastavica za znak se podize kad je broj nedativan, 
+; tj. kad je prvi bit sleva 1
 
 a: DB 2 ;        a = 2 = 0000.0010
 b: DB -3;        a = -3 = 111...
+; zbir je 100% oblika SF->1...
 c: DB 0 ; c = 0, Write into RAM, on Address &c
 
 start:
