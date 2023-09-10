f5f4f0db0e7c053deb01ab1b02d2c65963ceca44
partibrejkers <brejkersi@gmail.com>
2023-09-10 14:01:50 +0200

    naredba grananja...

---
+++ 03knjiga/05osnovno/p5.4 paran.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Program koji ispituje da li je uneti broj paran */
+int main() {
+    int a;
+    printf("Unesi broj: ");
+    scanf("%d", &a); 
+    /* Umesto %i moze i %d. %i je zato sto je podatak tipa int, a %d jer je u dekadnom brojevnom sistemu. */
+    if (a % 2 == 0) /* ako je ostatak prilikom deljenja sa 2 nula... */
+        printf("Broj %d je paran\n", a);
+    else /* ako nije nula... */
+        printf("Broj %d je neparan\n", a);
+    return 0; }
