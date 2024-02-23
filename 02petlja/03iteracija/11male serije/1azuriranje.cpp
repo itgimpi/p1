322aec03da1896ca5a559d749b334c66caef79f9
itgimpi <brejkersi@gmail.com>
2024-02-23 10:56:52 +0100

    ite

---
+++ 02petlja/03iteracija/11male serije/1azuriranje.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+// Ажурирање вредности променљивих
+int main() {
+/*
+x = 1; // azuriranje / update / promena vrednosti prom.
+cout << x << endl;
+x = 2;
+cout << x << endl;
+
+
+
+
+
+int x = 5; // update, warning!!! ++
+cout << x << endl;
+cout << x++ << endl;
+cout << x << endl;
+cout << ++x << endl;
+
+
+
+
+
+
+int PovecajZaDva(int x) { // update using funcion
+    return x + 2; } // <- the most important
+
+int main() {
+    int x = 0;
+    cout << "Pocetna vrednost x je " << x << endl;
+    x = PovecajZaDva(x);
+    cout << "Nova vrednost x je " << x << endl;
+    return 0; }
+
+
+
+
+
+void PovecajZaDva(int x) { // update only in function, not in main!!!
+   cout << "Vrednost x na ulazu u funkciju je " << x << endl;
+   x = x + 2;
+   cout << "Vrednost x na izlazu iz funkcije je " << x << endl; }
+
+int main() {
+    int x = 0
+    cout << "Pocetna vrednost x je " << x << endl;
+    PovecajZaDva(x);
+    cout << "Nova vrednost x je " << x << endl;
+}
+
+void PovecajZaDva(int& x) { // update in main, using addresses
+   x = x + 2; }
+
+int main() { 
+   int x = 0
+   cout << "Pocetna vrednost x je " << x << endl;
+   PovecajZaDva(x);
+   cout << "Nova vrednost x je " << x << endl; }
+
+
+
+
+void razmeni1(int a, int b) { // uncorrect swap 
+   int tmp = a;
+   a = b;
+   b = tmp;
+}
+
+void razmeni2(int& a, int& b) { // correct swap 
+   int tmp = a;
+   a = b;
+   b = tmp;
+}
+
+
+
+
+*/
+
+    return 0;  
+}
---
+++ 02petlja/03iteracija/11male serije/2osn stat.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+// Основне статистике
+int main() {
+/*
+
+broj1 + broj2 + broj3 + broj4 // zbir, suma, ...
+
+zbir = broj1; // zbir se akumulira
+...
+zbir = zbir + broj2;
+...
+zbir = zbir + broj3;
+...
+
+
+
+
+zbir = 0; // od nule
+zbir = zbir + broj1;
+zbir = zbir + broj2;
+...
+
+
+
+proizvod = 1; // neutralni el. za mnozenje
+proizvod = proizvod * broj1;
+proizvod = proizvod * broj2;
+...
+
+
+// min od 4 broja
+m = min(min(min(broj1, broj2), broj3), broj4)
+
+minimum = broj1; // prvi je najmanji
+minimum = min(minimum, broj2); // ako je drugi manji...
+minimum = min(minimum, broj3); // ...
+...
+
+
+
+minimum = broj1; // short if
+minimum = broj2 < minimum ? broj2 : minimum;
+minimum = broj3 < minimum ? broj3 : minimum;
+...
+
+
+
+minimum = broj1; // if
+if (broj2 < minimum)
+   minimum = broj2;
+else
+   minimum = minimum;
+if (broj3 < minimum)
+   minimum = broj3;
+else
+   minimum = minimum;
+...
+
+
+
+m = min({m1, m2, m3, m4, m5});
+
+*/    
+
+
+}
